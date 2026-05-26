4868f416e6928817faba684b6fe75a910387e207
Isaac Diaz <isaaccnkr@gmail.com>
2026-05-25 21:49:24 -0500

    feature: add database mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,37 @@
 import { Module } from '@nestjs/common';
-import { ProductModule } from './product/product.module';
+import { ProductModule } from './product/product.module'; // Único módulo de negocio
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const user = configService.get<string>('DB_USER');
+        const password = configService.get<string>('DB_PASSWORD');
+        const cluster = configService.get<string>('DB_CLUSTER');
+        const dbName = configService.get<string>('DB_NAME');
+
+        return {
+          uri: `mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`,
+        };
+      },
+    }),
+    ProductModule,
+  ],
   controllers: [],
   providers: [],
 })
