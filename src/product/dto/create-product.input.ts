81b3b898777d5050081ef8fb9474ec67bd93503b
Isaac Diaz <isaaccnkr@gmail.com>
2026-05-25 21:50:16 -0500

    feature: add validators for create product input

--- src/product/dto/create-product.input.ts
+++ src/product/dto/create-product.input.ts
@@ -1,14 +1,18 @@
-import { InputType, Field } from '@nestjs/graphql';
+import { InputType, Field, Float } from '@nestjs/graphql';
+import { MinLength, IsPositive, IsNotEmpty } from 'class-validator'; // Importamos validadores
 
 @InputType()
 export class CreateProductInput {
     @Field()
+    @IsNotEmpty({ message: 'Name could not be empty' })
+    @MinLength(3, { message: 'Name must be at least 3 characters long' })
     name!: string;
 
     @Field()
+    @IsNotEmpty()
     description!: string;
 
-    @Field()
-    price!: string;
-
+    @Field(() => Float)
+    @IsPositive({ message: 'Price must be a positive number' })
+    price!: number;
 }
